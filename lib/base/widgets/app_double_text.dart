da87f9e33ec3e2f0b7421582b5f7efc50323e13d
V-UNiQuE-V <panvalkarvallari@gmail.com>
2025-07-13 09:02:20 +0530

    copyWith Method

--- lib/base/widgets/app_double_text.dart
+++ lib/base/widgets/app_double_text.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/app_styles.dart';
 
 class AppDoubleText extends StatelessWidget {
-  const AppDoubleText({super.key, required this.bigText, required this.smallText});
+  const AppDoubleText({super.key, required this.bigText, required this.smallText}); // {} = you can choose (optional) which argument to pass which isn't required
   //Passed Text
   final String bigText;
   final String smallText;
@@ -19,7 +19,7 @@ class AppDoubleText extends StatelessWidget {
             onTap: () {
 
             },
-            child: Text(smallText, style: AppStyles.textStyle.copyWith(
+            child: Text(smallText, style: AppStyles.textStyle.copyWith( //copyWith method - helps to create new objects with changes in some of the parameters. Based on the object given to it, takes it's values as default values
               color: AppStyles.primaryColor
             ))
           )
